8d1527e9a685624e1d63bb74ff95229fc579cbfd
boybook <fangyizhou1997@qq.com>
2014-12-28 17:34:08 +0800

    ≥﹏≤

--- local/s2l.js
+++ local/s2l.js
@@ -1,4 +1,18 @@
+var a = 0;
 //现在是空的 XD,local部分就交给了,bb君
 function newLevel() {
-    ModPE.saveData("Try To Connect The Server");
+    //ModPE.showTipMessage("Try To Connect The Server");
+    address = Server.getAddress();
+    ModPE.showTipMessage("Try To Connect The Server\n" + address);
+    
 }	
+
+function modTick() {
+  a++
+  if(a>=20) {
+		 a=0;
+		 address = Server.getAddress();
+    ModPE.showTipMessage("Try To Connect The Server\n" + address); 
+		 }
+}
+ 
