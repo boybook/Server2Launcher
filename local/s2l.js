a5fcaf1086f89d0b779444f5ffe3ac36ea6b3b5d
boybook <fangyizhou1997@qq.com>
2014-12-29 01:38:21 +0800

    前10秒显示

--- local/s2l.js
+++ local/s2l.js
@@ -1,4 +1,5 @@
 var a = 0;
+var jointick = 0;
 //现在是空的 XD,local部分就交给了,bb君
 function newLevel() {
     //ModPE.showTipMessage("Try To Connect The Server");
@@ -11,8 +12,12 @@ function modTick() {
     a++
     if(a>=20) {
         a=0;
-        address = Server.getAddress();
-        ModPE.showTipMessage("Try To Connect The Server : " + address);
+        if(jointick <= 10) {
+            jointick++
+            address = Server.getAddress();
+            ModPE.showTipMessage("Trying To Connect The Server : " + address);
+        }
+        
     }
 }
  
